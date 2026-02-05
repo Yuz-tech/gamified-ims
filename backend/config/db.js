b7d021b351654093d4a7f68832fe6586537d9967
Julius <galejojulius@gmail.com>
2026-02-05 08:28:40 +0800

    User model and config

---
+++ backend/config/db.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoos');
+const connectDB = async() => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URI);
+        console.log('MongoDB Connected...');
+    } catch (err) {
+        console.log('MongoDB Connection error: ', err.message);
+        process.exit(1);
+    }
+};
+
+module.exports = connectDB;
