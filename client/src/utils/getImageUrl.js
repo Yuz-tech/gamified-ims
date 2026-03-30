8b148eccfbea0ea8096cf55c4705513efc71c0d0
Julius <2193136@slu.edu.ph>
2026-03-30 10:49:15 +0800

    changed IP

--- client/src/utils/getImageUrl.js
+++ client/src/utils/getImageUrl.js
@@ -10,7 +10,7 @@ export const getImageUrl = (path) => {
     }
 
     if (path.startsWith('/uploads/')) {
-        const API_BASE = process.env.REACT_APP_API_URL?.replace('/api','') || 'http://192.168.232.247:5000';
+        const API_BASE = process.env.REACT_APP_API_URL?.replace('/api','') || 'http://192.168.86.2:5000';
         return `${API_BASE}${path}`;
     }
 
