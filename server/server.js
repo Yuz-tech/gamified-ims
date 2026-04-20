c663659a982f874e9fc6e39ef0bbefea696ca98e
Julius <2193136@slu.edu.ph>
2026-04-20 14:47:26 +0800

    games

--- server/server.js
+++ server/server.js
@@ -27,12 +27,12 @@ const corsOptions = {
     if (!origin) return callback(null, true);
 
     const allowedOrigins = [
-      'http://192.168.232.247:3000',
-      'http://192.168.232.247:5000',
-      'http://192.168.232.247:5173',
-      'http://192.168.232.247:27017',
+      'http://localhost:3000',
+      'http://localhost:5000',
+      'http://localhost:5173',
+      'http://localhost:27017',
       process.env.FRONTEND_URL,
-      '192.168.232.247:3000'
+      'localhost:3000'
     ].filter(Boolean);
 
     if (allowedOrigins.includes(origin)) {
