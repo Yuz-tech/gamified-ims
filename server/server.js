16dc4ba701e87197c7edb7c44b359def8f0d217d
Julius <2193136@slu.edu.ph>
2026-05-03 17:44:57 +0800

    server.js updated

--- server/server.js
+++ server/server.js
@@ -53,13 +53,6 @@ app.use(express.urlencoded({ extended: true }));
 // Static files (uploaded images)
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-  app.get('*', (req,res) => {
-    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
-  });
-}
-
 app.set('trust proxy', true);
 
 // Database connection
