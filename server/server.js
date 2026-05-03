2d20c29287405bf8ff4da39841cff10230d27c19
Julius <2193136@slu.edu.ph>
2026-05-03 15:05:13 +0800

    lolz

--- server/server.js
+++ server/server.js
@@ -27,12 +27,12 @@ const corsOptions = {
     if (!origin) return callback(null, true);
 
     const allowedOrigins = [
-      'http://127.0.0.1:3000',
-      'http://127.0.0.1:5000',
-      'http://127.0.0.1:5173',
-      'http://127.0.0.1:27017',
+      'http://0.0.0.0:3000',
+      'http://0.0.0.0:5000',
+      'http://0.0.0.0:5173',
+      'http://0.0.0.0:27017',
       process.env.FRONTEND_URL,
-      '127.0.0.1:3000'
+      '0.0.0.0:3000'
     ].filter(Boolean);
 
     if (allowedOrigins.includes(origin)) {
