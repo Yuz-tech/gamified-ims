da1c3bc3e0578c964e44f0ad1a835cd6634cf124
Julius <galejojulius@gmail.com>
2026-05-07 14:48:37 +0800

    new avatar and mascot

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -19,18 +19,10 @@ const Profile = () => {
     });
 
     const avatarOptions = [
-        '/avatars/bumblebee.png',
-        '/avatars/optimum pride.png',
-        '/avatars/luffy.jpg',
-        '/avatars/goku.jpg',
-        '/avatars/pikachu.png',
-        '/avatars/baymax.png',
-        '/avatars/charm.jpg',
-        '/avatars/bulb.jpg',
-        '/avatars/squirtle.png',
-        '/avatars/jake.png',
-        '/avatars/yoshi.png',
-        '/avatars/stitch.png'
+        '/avatars/hachi.png',
+        '/avatars/hachikun.png',
+        '/avatars/hachihead.png',
+        '/avatars/hachikun2.png'
     ];
 
     useEffect(() => {
